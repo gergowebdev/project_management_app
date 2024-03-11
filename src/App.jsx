1ca290439f0039764509198ee7b33b46dc1d85c5
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-03-11 20:43:32 +0100

    Adding a Task component

--- src/App.jsx
+++ src/App.jsx
@@ -54,12 +54,29 @@ function App() {
         });
     }
 
+    function handleDeleteProject() {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+                projects: prevState.projects.filter(
+                    (project) => project.id !== prevState.selectedProjectId
+                ),
+            };
+        });
+    }
+
     // finding an element in an array by ID //
     const selectedProject = projectsState.projects.find(
         (project) => project.id === projectsState.selectedProjectId
     );
 
-    let content = <SelectedProject project={selectedProject} />;
+    let content = (
+        <SelectedProject
+            project={selectedProject}
+            onDelete={handleDeleteProject}
+        />
+    );
 
     if (projectsState.selectedProjectId === null) {
         content = (
